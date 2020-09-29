fcf6de03940da838ec877dc71880e3b21b0ef0b4
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-09-29 19:41:52 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,32 @@
 # code your #valid_move? method here
 
+def valid_move? (board, index)
+  position = position_taken?(board, index) 
+  
+  if position == false && index.between?(0, 8)
+    true
+    
+  else
+    false
+  end
+  
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+   
+  else
+    true
+ end
+end
+
+def between?(index, min, max)
+  if min <= index <= max
+    true
+  else
+    false
+  end
+end
